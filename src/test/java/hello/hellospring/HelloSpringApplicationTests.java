73eee58a036d93274aed99d0845c1b358fe21b32
jameshasung <117165485+jameshasung@users.noreply.github.com>
2023-05-30 15:00:36 +0900

    230530 spring start

---
+++ src/test/java/hello/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package hello.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+	public static void main(String[] args){
+		SpringApplication.run(HelloSpringApplicationTests.class, args);
+	}
+	@Test
+	void contextLoads() {
+	}
+
+}
